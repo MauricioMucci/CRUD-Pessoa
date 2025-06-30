7ce96558dcddfaed8339f6c61490950f7be45739
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-06-29 21:25:37 -0300

    feat(frontend): initialize React project with Vite and ESLint configuration

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,41 @@
+export interface EnderecoDTO {
+    cep: number;
+    rua: string;
+    numero: number;
+    cidade: string;
+    estado: string;
+}
+
+export interface PessoaInputDTO {
+    nome: string;
+    nascimento: string; // Formato YYYY-MM-DD
+    cpf: string;
+    email: string;
+    endereco: EnderecoDTO;
+}
+
+export interface PessoaOutputDTO {
+    nome: string;
+    nascimento: string; // Formato YYYY-MM-DD
+    cpf: string;
+    email: string;
+    endereco: EnderecoDTO;
+    dataHoraInclusaoRegistro: string; // Formato ISO 8601
+    dataHoraUltimaAlteracaoRegistro: string; // Formato ISO 8601
+}
+
+export interface ViaCepDTO {
+    cep: string;
+    rua: string;
+    cidade: string;
+    estado: string;
+}
+
+export interface CreatePessoaDTO {
+    idPessoa: number;
+    mensagem: string;
+}
+
+export interface DeletePessoaDTO {
+    mensagem: string;
+}
